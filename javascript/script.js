4037fad8dcff675565af1632055b8993979e2b57
Scharly OPITZ <scharly.opitz@gmail.com>
2023-08-31 21:30:00 +0200

    Changement du design de la page de presentation + animations

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,4 @@
 export default function script() {
-    // // *********** Changement d'image au survol de la souris ***********
-
-    // const imageDisplay = document.querySelector(".image-display");
-
-    // const liens = document.querySelectorAll("header a");
-
-    // liens.forEach((lien) => {
-    //     const pays = lien.className;
-
-    //     lien.addEventListener("mouseover", function () {
-    //         imageDisplay.style.backgroundImage = `url(images/${pays}.jpg)`;
-    //     });
-
-    //     lien.addEventListener("mouseleave", function () {
-    //         imageDisplay.style.backgroundImage = "";
-    //     });
-    // });
-
-    // Affichage permanent container-image
-
-    const containerImage = document.querySelector(".container-image");
-
-    setTimeout(() => containerImage.classList.add("not-hide"), 5000);
-
     // ************************* Animation au reload *************************
 
     window.addEventListener("load", function () {
--- javascript/socialMedia.js
+++ javascript/socialMedia.js
@@ -1,27 +1,27 @@
 export default function socialMedia() {
-    // Instagram
+    // // Instagram
 
-    const instagram = document.querySelector(".icon-contact .instagram");
+    // const instagram = document.querySelector(".icon-contact .instagram");
 
-    instagram.addEventListener("mouseover", function () {
-        imageDisplay.style.backgroundImage = `url(https://www.numerama.com/wp-content/uploads/2021/08/logo-instagram.jpg)`;
-    });
+    // instagram.addEventListener("mouseover", function () {
+    //     imageDisplay.style.backgroundImage = `url(https://www.numerama.com/wp-content/uploads/2021/08/logo-instagram.jpg)`;
+    // });
 
-    instagram.addEventListener("mouseleave", function () {
-        imageDisplay.style.backgroundImage = "";
-    });
+    // instagram.addEventListener("mouseleave", function () {
+    //     imageDisplay.style.backgroundImage = "";
+    // });
 
-    // GitHub
+    // // GitHub
 
-    const gitHub = document.querySelector(".icon-contact .github");
+    // const gitHub = document.querySelector(".icon-contact .github");
 
-    gitHub.addEventListener("mouseover", function () {
-        imageDisplay.style.backgroundImage = `url(https://editor.analyticsvidhya.com/uploads/765900ba9-article-200807-github-gitguardbody-text.jpg)`;
-    });
+    // gitHub.addEventListener("mouseover", function () {
+    //     imageDisplay.style.backgroundImage = `url(https://editor.analyticsvidhya.com/uploads/765900ba9-article-200807-github-gitguardbody-text.jpg)`;
+    // });
 
-    gitHub.addEventListener("mouseleave", function () {
-        imageDisplay.style.backgroundImage = "";
-    });
+    // gitHub.addEventListener("mouseleave", function () {
+    //     imageDisplay.style.backgroundImage = "";
+    // });
 
     // Affichage permanent réseaux sociaux
 
